f2c62deb11e208e9bd16177b65e76ffc272a00c4
Emrecan Balgün <deathlygrave@gmail.com>
2021-09-19 17:16:10 +0300

    added all files

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+// Import components
+import Form from "./components/Form"
+import TodoList from "./components/TodoList"
+
+
+function App() {
+  // State stuff
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // Run once when start
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  // Use effect
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status])
+
+  // Functions
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true));
+          break;
+      case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  // Save to Local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]))
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Emron's Todo List</h1>
+      </header>
+      <Form 
+        todos={todos} 
+        setTodos={setTodos} 
+        inputText={inputText} 
+        setInputText={setInputText}
+        setStatus={setStatus}    
+      />
+      <TodoList 
+        setTodos={setTodos} 
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
